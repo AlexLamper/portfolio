33f65404d1c1193aca187c060a49ed3df1ba2ae8
AlexLamper <143872753+AlexLamper@users.noreply.github.com>
2023-12-22 22:48:36 +0100

    feed.xml gen script

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -6,8 +6,8 @@ const matter = require('gray-matter')
 async function generate() {
   const feed = new RSS({
     title: 'Alex Lamper',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml',
+    site_url: 'https://alexlamper.nl',
+    feed_url: 'https://alexlamper.nl/feed.xml',
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
