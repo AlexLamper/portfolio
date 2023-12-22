3e529bca941fdf2cda8267a5d9be2e09b957f9d0
AlexLamper <143872753+AlexLamper@users.noreply.github.com>
2023-12-22 22:51:18 +0100

    Updated Footer Text

--- theme.config.js
+++ theme.config.js
@@ -4,8 +4,8 @@ export default {
   footer: (
     <footer>
       <small>
-        <time>{YEAR}</time> © Your Name.
-        <a href="/feed.xml">RSS</a>
+        <time>{YEAR}</time> © Alex Lamper.
+        <a href="https://lucerny.nl">lucerny</a>
       </small>
       <style jsx>{`
         footer {
